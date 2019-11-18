9614f9b527fbff491b098ba3f55762fb6fbe8eab
jack-debug <55571956+jack-debug@users.noreply.github.com>
2019-11-18 12:39:33 +1100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,5 +7,12 @@ url = 'http://prio.2b2t.dev/'
 response = requests.get(url)
 soup = bs4.BeautifulSoup(response.text, 'html.parser')
 soup.findAll('div')
-one_a_tag = soup.findAll('div')[2]
-print(one_a_tag)
+prioq = soup.findAll('div')[2]
+
+url = 'http://2b2t.io/'
+response = requests.get(url)
+soup = bs4.BeautifulSoup(response.text, 'html.parser')
+soup.findAll('h1')
+queue = soup.findAll('h1')
+
+print('2b2t normal queue is '+ queue + '. 2b2t priority queue is ' + prioq + '.')
